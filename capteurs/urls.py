e2f5d83711191e72cf761f0c3eb6eb443ade1a80
fskei <keif1201@usherbrooke.ca>
2025-07-03 01:17:18 -0400

    Ajout du bouton de deconnexion

--- capteurs/urls.py
+++ capteurs/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),  # ta page d'accueil 
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),  
     path('home/', views.home, name='home'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     
 ]
