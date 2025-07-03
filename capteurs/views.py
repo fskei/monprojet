0939dad562bf3ec4703c91395862497b1d40708a
fskei <keif1201@usherbrooke.ca>
2025-07-02 23:34:26 -0400

    Ajout dun super user sur la page dacceuil

--- capteurs/views.py
+++ capteurs/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+
 
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Bienvenue sur la page d'accueil de l'application  !")
+
+def index(request):
+    return redirect('/admin/login/')
 
